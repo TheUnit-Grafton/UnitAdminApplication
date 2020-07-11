c65472b44877ac8ecf3c865890dbbbb0fdc4ac06
Steve Teece <sgteece@hotmail.com>
2020-07-11 12:02:07 +1000

    COmplete SignUp.... now creating user records in db

--- UnitAdmin/Models/AppUser.cs
+++ UnitAdmin/Models/AppUser.cs
@@ -8,5 +8,12 @@ namespace UnitAdmin.Models
 {
     public class AppUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
     }
 }
