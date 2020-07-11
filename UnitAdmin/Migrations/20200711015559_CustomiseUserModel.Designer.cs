3ac940481da1a245759e5b7e81f7fdc54024bb71
Steve Teece <sgteece@hotmail.com>
2020-07-11 12:02:07 +1000

    COmplete SignUp.... now creating user records in db

--- UnitAdmin/Migrations/20200711015559_CustomiseUserModel.Designer.cs
+++ UnitAdmin/Migrations/20200711015559_CustomiseUserModel.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UnitAdmin.Data;
 
 namespace UnitAdmin.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200711015559_CustomiseUserModel")]
+    partial class CustomiseUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
