f0751e67132da0564841b6489c4e7d3581c33ecc
Steve Teece <sgteece@hotmail.com>
2021-01-27 14:13:05 +1100

    Change parts update to use UpdatePartAsync() instead of SavePartAsync()

--- UnitAdmin/Pages/Parts/EditPart.razor.cs
+++ UnitAdmin/Pages/Parts/EditPart.razor.cs
@@ -1,33 +1,13 @@
 ﻿using DataLibrary.Models;
 using Microsoft.AspNetCore.Components;
 using System.Threading.Tasks;
+using UnitAdmin.Shared;
 
 namespace UnitAdmin.Pages.Parts
 {
     public partial class EditPart : ComponentBase
     {
-        private PartModel partToEdit = new PartModel();
-
-        protected override async Task OnParametersSetAsync()
-        {
-            partToEdit = await parts.GetPartByIdAsync(id);
-        }
-
-        private async Task UpdateRecord()
-        {
-            await parts.SavePartAsync(partToEdit);
-            navMan.NavigateTo("/parts");
-        }
-
-        private void CancelUpdate()
-        {
-            navMan.NavigateTo("/parts");
-        }
-
-        private void DeleteRecord()
-        {
-            parts.DeletePart(partToEdit);
-            navMan.NavigateTo("/parts");
-        }
+        
+        
     }
 }
