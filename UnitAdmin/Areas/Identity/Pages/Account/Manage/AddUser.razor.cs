f10372d23e4f64aad4afd39969e1d7b432ff2a3b
Steve Teece <sgteece@hotmail.com>
2021-01-29 13:09:03 +1100

    Modify user controls. Commit created to switch to a new branch for another issue.

---
+++ UnitAdmin/Areas/Identity/Pages/Account/Manage/AddUser.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnitAdmin.Data;
+using UnitAdmin.Models;
+
+namespace UnitAdmin.Areas.Identity.Pages.Account.Manage
+{
+    public partial class AddUser : ComponentBase
+    {
+        [Inject] NavigationManager _nav { get; set; }
+        [Inject] SecurityService _security { get; set; }
+        [Inject] RevalidatingIdentityAuthenticationStateProvider<AppUser> _riasp { get; set; }
+
+        [Parameter] public AppUser newUser { get; set; } = new AppUser();
+
+
+        private async Task CreateUser(AppUser newUser)
+        {
+            newUser = await _security.CreateUser(newUser);
+            _nav.NavigateTo("admin/manage/users");
+        }
+
+        public void CancelClicked()
+        {
+            _nav.NavigateTo("/admin/manage/users");
+        }
+    }
+}
